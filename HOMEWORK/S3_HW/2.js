6aa63f75366a61625cc68ab7a5e47727a6e4705a
NikolayKartaev <kartaevgeek@gmail.com>
2023-07-11 14:40:53 +0300

    WEB S3

--- HOMEWORK/S3_HW/2.js
+++ HOMEWORK/S3_HW/2.js
@@ -1,5 +1,5 @@
 function greeting(yourName) {
-    console.log (`Здравствуйте, дорогой ${yourName}!`);
+    console.log(`Здравствуйте, дорогой ${yourName}!`);
 }
 
 greeting(prompt("Введите ваше имя:"));
